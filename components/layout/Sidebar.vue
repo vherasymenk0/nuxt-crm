551c354db59cb6364100e10b556b90ec340a9d16
vherasymenko <manqarium@gmail.com>
2024-07-16 09:23:29 +0300

    [Feature] add loader

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -3,7 +3,7 @@
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full w-full relative">
     <NuxtLink class="mb-10 block" to="/">
-      <NuxtImg class="mx-auto mt-4" src="/logo.svg" width="140px"/>
+      <NuxtImg alt="Logo" class="mx-auto mt-4" src="/logo.svg" width="140px"/>
       <button class="absolute top-2 right-3 transition-colors hover:text-primary">
         <Icon name="line-md:logout" size="20px"/>
       </button>
